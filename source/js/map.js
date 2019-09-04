38b2e626cb248245eff977e22b34238510afc24f
Otkrovennost <Otkrovennost@mail.ru>
2019-09-04 22:20:55 +0300

    метка на карте

--- source/js/map.js
+++ source/js/map.js
@@ -14,38 +14,36 @@ var mapPoster = mapBlock.querySelector(".map__picture");
 var mapPreviewPlacemark = mapBlock.querySelector(".map__placemark--preview");
 var template = "<div class=\"map__placemark\"></div>";
 
+var breakpoint;
+var setBreakpoint = function () {
+  breakpoint = 'mobile';
+  if (window.innerWidth >= 768) {
+    breakpoint = 'tablet';
+  }
+  if (window.innerWidth >= 1300) {
+    breakpoint = 'desktop';
+  }
+};
+setBreakpoint();
+
 window.ymaps.ready(function () {
   var map = new window.ymaps.Map(mapBlock.id, {
-    center: centers.mobile,
+    center: centers[breakpoint],
     controls: [],
-    zoom: zooms.mobile
+    zoom: zooms[breakpoint]
   });
-  var setCoords = function (width) {
-    var center = centers.mobile;
-    var zoom = zooms.mobile;
-    if (width >= 768) {
-      center = centers.tablet;
-      zoom = zooms.tablet;
-    }
-    if (width >= 1300) {
-      center = centers.desktop;
-      zoom = zooms.desktop;
-    }
-    console.log(center, zoom);
-    map.setCenter(center, zoom);
-  };
 
-  setCoords();
   map.geoObjects.add(new window.ymaps.Placemark([59.93886, 30.32273], null, {
     iconLayout: window.ymaps.templateLayoutFactory.createClass(template)
   }));
   map.behaviors.disable("scrollZoom");
 
   window.addEventListener("resize", function () {
-    setCoords(window.innerWidth);
+    setBreakpoint();
+    map.setCenter(centers[breakpoint], zooms[breakpoint]);
   });
 
   // Скрытие замещающего изображения
   mapPoster.classList.add("hidden");
-  mapPreviewPlacemark.add("hidden");
+  mapPreviewPlacemark.classList.add("hidden");
 });
